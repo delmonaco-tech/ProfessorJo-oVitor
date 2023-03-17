ae4f5c8c6d098fff56b7a44b391652015a31d4b7
delmonaco <pedro.delmonaco@edu.unifil.br>
2023-03-16 21:22:03 -0300

    exercicio1

---
+++ problema1.java
@@ -0,0 +1,19 @@
+public class problema1 {
+
+    public static void main(String[] args) {
+
+        String[] arra = new String[5];
+
+        arra[0] = " +\"\"\"\"\"+ ";
+        arra[1] = "[| o o |]";
+        arra[2] = " |  ^  |";
+        arra[3] = " | '-' |";
+        arra[4] = " +-----+";
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arra[i]);
+           }
+         
+          }
+         
+         }
